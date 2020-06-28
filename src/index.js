f5958811baf78609fb88a3d927f335f00b641e2a
white0221 <whiterm.r0221@gmail.com>
2020-06-28 16:18:54 +0900

    fixed format

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        { /* todo */ }
+        {/* todo */}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -22,21 +22,21 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{ status }</div>
+        <div className="status">{status}</div>
         <div className="board-row">
-          { this.renderSquare(0) }
-          { this.renderSquare(1) }
-          { this.renderSquare(2) }
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          { this.renderSquare(3) }
-          { this.renderSquare(4) }
-          { this.renderSquare(5) }
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          { this.renderSquare(6) }
-          { this.renderSquare(7) }
-          { this.renderSquare(8) }
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
@@ -51,8 +51,8 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{ /*status*/ }</div>
-          <div>{ /*todo*/ }</div>
+          <div>{/*status*/}</div>
+          <div>{/*todo*/}</div>
         </div>
       </div>
     );
